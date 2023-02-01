2b1eb57aabe8e406c8b9aa603cc04f5d59027e3c
OgurchickRick <artem.derbin.04@list.ru>
2023-02-01 17:26:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сторону квадрата: ");
+            int side = Convert.ToInt32(Console.ReadLine());
+            int perimeter = 4 * side;
+            Console.WriteLine($"Периметр квадрата равен: {perimeter}");
+        }
+    }
+}
